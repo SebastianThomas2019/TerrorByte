01bd65b1afb6e2ed37b85e1bb7a7709d8576f4d3
sebastian <sebastian.thomas@ero.eu>
2025-09-08 14:26:40 +0200

    commented code out

--- src/TerrorByte.cpp
+++ src/TerrorByte.cpp
@@ -1,173 +1,173 @@
-#include "TerrorByte.h"
-#include "protothreads.h"
-#include "pins.h"
-#include <iostream>
-#include <string.h>
+// #include "TerrorByte.h"
+// #include "protothreads.h"
+// #include "pins.h"
+// #include <iostream>
+// #include <string.h>
 
 
-TerrorByte::TerrorByte()
-    : m_poweredON(false)
-    , ptr_statusRgb(new RgbLed(1,2,3))
-    , m_spaceLeftSide(0)
-    , m_spaceRightSide(0)
-    , m_spaceFrontSide(0)
-    , m_spaceBackSide(0)
-{
-    pinMode(pin::LEDS_FRONT_AND_BACK, OUTPUT);
-    pinMode(pin::BUZZER, OUTPUT);
-};
+// TerrorByte::TerrorByte()
+//     : m_poweredON(false)
+//     , ptr_statusRgb(new RgbLed(1,2,3))
+//     , m_spaceLeftSide(0)
+//     , m_spaceRightSide(0)
+//     , m_spaceFrontSide(0)
+//     , m_spaceBackSide(0)
+// {
+//     pinMode(pin::LEDS_FRONT_AND_BACK, OUTPUT);
+//     pinMode(pin::BUZZER, OUTPUT);
+// };
 
-void TerrorByte::powerButtonPressed()    
-{
-    if (m_poweredON)
-    {
-        m_poweredON = false;
-        ptr_statusRgb->setColor(RgbLed::WHITE);
+// void TerrorByte::powerButtonPressed()    
+// {
+//     if (m_poweredON)
+//     {
+//         m_poweredON = false;
+//         ptr_statusRgb->setColor(RgbLed::WHITE);
 
-        digitalWrite(pin::LEDS_FRONT_AND_BACK, LOW);
-    }
-    else
-    {
-        m_poweredON = true;
-        ptr_statusRgb->setColor(RgbLed::GREEN);
+//         digitalWrite(pin::LEDS_FRONT_AND_BACK, LOW);
+//     }
+//     else
+//     {
+//         m_poweredON = true;
+//         ptr_statusRgb->setColor(RgbLed::GREEN);
 
-        digitalWrite(pin::LEDS_FRONT_AND_BACK, HIGH);
-    }
-};
+//         digitalWrite(pin::LEDS_FRONT_AND_BACK, HIGH);
+//     }
+// };
 
-void TerrorByte::getDistances(int array[4])
-{
-    measureDistance();
+// void TerrorByte::getDistances(int array[4])
+// {
+//     measureDistance();
 
-    if (sizeof(array) >= 4 * sizeof(int))
-    int distances[4]
-    {
-        [LEFT]  = m_spaceLeftSide,
-        [RIGHT] = m_spaceRightSide,
-        [FRONT] = m_spaceFrontSide,
-        [BACK]  = m_spaceBackSide
-    };
+//     if (sizeof(array) >= 4 * sizeof(int))
+//     int distances[4]
+//     {
+//         [LEFT]  = m_spaceLeftSide,
+//         [RIGHT] = m_spaceRightSide,
+//         [FRONT] = m_spaceFrontSide,
+//         [BACK]  = m_spaceBackSide
+//     };
 
-    return;
-};
+//     return;
+// };
 
-bool   TerrorByte::driveInDirection(direction_t direction)
-{
-    //This methode still needs to be implemented.
-    std::string directionStr = "";
+// bool   TerrorByte::driveInDirection(direction_t direction)
+// {
+//     //This methode still needs to be implemented.
+//     std::string directionStr = "";
 
-    switch (direction)
-    {
-    case LEFT:
-        directionStr = "Left";
-        break;
+//     switch (direction)
+//     {
+//     case LEFT:
+//         directionStr = "Left";
+//         break;
     
-    case RIGHT:
-        directionStr = "Right";
-        break;
+//     case RIGHT:
+//         directionStr = "Right";
+//         break;
     
-    case FRONT:
-        directionStr = "Forward";
-        break;
+//     case FRONT:
+//         directionStr = "Forward";
+//         break;
 
-    case BACK:
-        directionStr = "Backwards";
-        break;
+//     case BACK:
+//         directionStr = "Backwards";
+//         break;
     
-    case FRONT_LEFT:
-        directionStr = "Left-Forward";
-        break;
+//     case FRONT_LEFT:
+//         directionStr = "Left-Forward";
+//         break;
     
-    case FRONT_RIGHT:
-        directionStr = "Right-Forward";
-        break;
+//     case FRONT_RIGHT:
+//         directionStr = "Right-Forward";
+//         break;
     
-    case BACK_LEFT:
-        directionStr = "Left-Backwards";
-        break;
+//     case BACK_LEFT:
+//         directionStr = "Left-Backwards";
+//         break;
     
-    case BACK_RIGHT:
-        directionStr = "Right-Backwards";
-        break;
+//     case BACK_RIGHT:
+//         directionStr = "Right-Backwards";
+//         break;
 
-    default:
-        return false;
+//     default:
+//         return false;
     
-    return true;
-    }
+//     return true;
+//     }
 
-    std::cout << "Now driving: ";
-};
+//     std::cout << "Now driving: ";
+// };
 
-void   TerrorByte::scanForTarget()
-{
-    //ToDo
-}
+// void   TerrorByte::scanForTarget()
+// {
+//     //ToDo
+// }
 
-bool   TerrorByte::isActive()
-{
-    return m_poweredON;
-}
+// bool   TerrorByte::isActive()
+// {
+//     return m_poweredON;
+// }
 
-void  TerrorByte::measureDistance()
-{
-    int sensorPins[4];
+// void  TerrorByte::measureDistance()
+// {
+//     int sensorPins[4];
     
-    sensorPins[FRONT] = pin::ULTRASONIC_SENSOR_FRONT;
-    sensorPins[BACK]  = pin::ULTRASONIC_SENSOR_BACK;
-    sensorPins[LEFT]  = pin::ULTRASONIC_SENSOR_LEFT;
-    sensorPins[RIGHT] = pin::ULTRASONIC_SENSOR_RIGHT;
+//     sensorPins[FRONT] = pin::ULTRASONIC_SENSOR_FRONT;
+//     sensorPins[BACK]  = pin::ULTRASONIC_SENSOR_BACK;
+//     sensorPins[LEFT]  = pin::ULTRASONIC_SENSOR_LEFT;
+//     sensorPins[RIGHT] = pin::ULTRASONIC_SENSOR_RIGHT;
     
-    int *sensorData[4];
+//     int *sensorData[4];
 
-    sensorData[FRONT] = &m_spaceFrontSide;
-    sensorData[BACK]  = &m_spaceBackSide;
-    sensorData[LEFT]  = &m_spaceLeftSide;
-    sensorData[RIGHT] = &m_spaceRightSide;
+//     sensorData[FRONT] = &m_spaceFrontSide;
+//     sensorData[BACK]  = &m_spaceBackSide;
+//     sensorData[LEFT]  = &m_spaceLeftSide;
+//     sensorData[RIGHT] = &m_spaceRightSide;
 
-    for (int i = 0; i < 4; i++)
-    {
-        long duration = 0;
+//     for (int i = 0; i < 4; i++)
+//     {
+//         long duration = 0;
 
-        pinMode(sensorPins[i], OUTPUT);
-        digitalWrite(sensorPins[i], LOW);
-        delayMicroseconds(2);
-        digitalWrite(sensorPins[i], HIGH);
-        delayMicroseconds(5);
-        digitalWrite(sensorPins[i], LOW);
+//         pinMode(sensorPins[i], OUTPUT);
+//         digitalWrite(sensorPins[i], LOW);
+//         delayMicroseconds(2);
+//         digitalWrite(sensorPins[i], HIGH);
+//         delayMicroseconds(5);
+//         digitalWrite(sensorPins[i], LOW);
 
-        pinMode(sensorPins[i], INPUT);
-        duration = pulseIn(sensorPins[i], HIGH);
+//         pinMode(sensorPins[i], INPUT);
+//         duration = pulseIn(sensorPins[i], HIGH);
 
-        *sensorData[i] = (duration / 29 / 2);
-    }
+//         *sensorData[i] = (duration / 29 / 2);
+//     }
 
-};
+// };
 
-void   TerrorByte::activateBuzzer(int hz, int duration)
-{
-    if (!duration)
-    {
-        tone(pin::BUZZER, duration);
-        return;
-    }
-    //else
-    //{
-        //ToDo
-    //}
-};
+// void   TerrorByte::activateBuzzer(int hz, int duration)
+// {
+//     if (!duration)
+//     {
+//         tone(pin::BUZZER, duration);
+//         return;
+//     }
+//     //else
+//     //{
+//         //ToDo
+//     //}
+// };
 
-bool TerrorByte::beepingBuzzer(int hz, int duration, int sleepTime)
-{
-    //ToDo
-};
+// bool TerrorByte::beepingBuzzer(int hz, int duration, int sleepTime)
+// {
+//     //ToDo
+// };
 
-void   TerrorByte::disableBuzzer()
-{
-    //This methode still needs to be implemented.
-};
+// void   TerrorByte::disableBuzzer()
+// {
+//     //This methode still needs to be implemented.
+// };
 
-void TerrorByte::katapult()
-{
+// void TerrorByte::katapult()
+// {
 
-};
+// };
--- src/TerrorByte.h
+++ src/TerrorByte.h
@@ -1,48 +1,48 @@
-#ifndef TERRORBYTE
-#define TERRORBYTE
-# define EVER ;;
+// #ifndef TERRORBYTE
+// #define TERRORBYTE
+// # define EVER ;;
 
-#include "RgbLed.h"
-#include <Arduino.h>
+// #include "RgbLed.h"
+// #include <Arduino.h>
 
-class TerrorByte
-{
-public:
-    enum direction_t
-    {
-        LEFT,
-        RIGHT,
-        FRONT,
-        BACK,
-        FRONT_LEFT,
-        FRONT_RIGHT,
-        BACK_LEFT,
-        BACK_RIGHT
-    };
+// class TerrorByte
+// {
+// public:
+//     enum direction_t
+//     {
+//         LEFT,
+//         RIGHT,
+//         FRONT,
+//         BACK,
+//         FRONT_LEFT,
+//         FRONT_RIGHT,
+//         BACK_LEFT,
+//         BACK_RIGHT
+//     };
 
-    TerrorByte();
+//     TerrorByte();
 
-    void    powerButtonPressed();
-    void    getDistances(int array[4]);
-    bool    driveInDirection(direction_t direction);
-    void    scanForTarget();
-    bool    isActive();
-    void    katapult();
+//     void    powerButtonPressed();
+//     void    getDistances(int array[4]);
+//     bool    driveInDirection(direction_t direction);
+//     void    scanForTarget();
+//     bool    isActive();
+//     void    katapult();
 
-private:
-    void    measureDistance();
-    void    activateBuzzer(int hz, int duration = 0);
-    bool    beepingBuzzer(int hz, int duration, int sleepTime);
-    void    disableBuzzer();
+// private:
+//     void    measureDistance();
+//     void    activateBuzzer(int hz, int duration = 0);
+//     bool    beepingBuzzer(int hz, int duration, int sleepTime);
+//     void    disableBuzzer();
 
-    const int DURATION_FOR_MOVEMENT = 3; // seconds
-    const int FREQUENZ_BUZZER_DRIVING_BACKWARDS = 950; //Hz
+//     const int DURATION_FOR_MOVEMENT = 3; // seconds
+//     const int FREQUENZ_BUZZER_DRIVING_BACKWARDS = 950; //Hz
 
-    RgbLed     *ptr_statusRgb;
-    bool        m_poweredON;
-    int         m_spaceLeftSide;
-    int         m_spaceRightSide;
-    int         m_spaceFrontSide;
-    int         m_spaceBackSide;
-};
-#endif
+//     RgbLed     *ptr_statusRgb;
+//     bool        m_poweredON;
+//     int         m_spaceLeftSide;
+//     int         m_spaceRightSide;
+//     int         m_spaceFrontSide;
+//     int         m_spaceBackSide;
+// };
+// #endif
